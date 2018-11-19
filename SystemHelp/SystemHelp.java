b585a5723de976d2e85cf88ce55fe2b5a2c947ec
deafmex1995 <ec8155@g.rit.edu>
2018-11-19 15:52:00 -0500

    Add files via upload
    This folder has two files: EdgeConvertGUI and SystemHelp. You need to replace old EdgeConvertGUI with new one.

---
+++ SystemHelp/SystemHelp.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+import javax.swing.filechooser.FileFilter;
+import java.io.*;
+import java.util.*;
+import java.lang.reflect.*;
+
+public class SystemHelp extends JFrame {
+
+	public SystemHelp(){
+		super("System Help");
+		setSize(300,300);
+		setVisible(true);
+	}
+
+}
