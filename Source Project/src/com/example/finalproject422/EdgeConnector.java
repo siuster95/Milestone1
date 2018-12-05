86effedc731f4e69cdfd89f8b9cf780a14f3fb66
Johnny Siu <jxs7205@rit.edu>
2018-12-05 16:57:20 -0500

    Made some changes to EdgeConnector

--- Source Project/src/com/example/finalproject422/EdgeConnector.java
+++ Source Project/src/com/example/finalproject422/EdgeConnector.java
@@ -1,6 +1,9 @@
 package com.example.finalproject422;
 
 import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
+import javax.swing.*;
 
 public class EdgeConnector {
    private int numConnector, endPoint1, endPoint2;
@@ -18,6 +21,67 @@ public class EdgeConnector {
       isEP2Field = false;
       isEP1Table = false;
       isEP2Table = false;
+   }
+
+   public void resolveConnectors (EdgeField[] fields, EdgeTable[] tables, String fileName) {
+      int fieldIndex = 0, table1Index = 0, table2Index = 0;
+         fieldIndex = -1;
+         for (int fIndex = 0; fIndex < fields.length; fIndex++) { //search fields array for endpoints
+            if (endPoint1 == fields[fIndex].getNumFigure()) { //found endPoint1 in fields array
+               isEP1Field = true; //set appropriate flag
+               fieldIndex = fIndex; //identify which element of the fields array that endPoint1 was found in
+            }
+            if (endPoint2 == fields[fIndex].getNumFigure()) { //found endPoint2 in fields array
+               isEP2Field = true; //set appropriate flag
+               fieldIndex = fIndex; //identify which element of the fields array that endPoint2 was found in
+            }
+         }
+         for (int tIndex = 0; tIndex < tables.length; tIndex++) { //search tables array for endpoints
+            if (endPoint1 == tables[tIndex].getNumFigure()) { //found endPoint1 in tables array
+               isEP1Table = true; //set appropriate flag
+               table1Index = tIndex; //identify which element of the tables array that endPoint1 was found in
+            }
+            if (endPoint2 == tables[tIndex].getNumFigure()) { //found endPoint1 in tables array
+               isEP2Table = true; //set appropriate flag
+               table2Index = tIndex; //identify which element of the tables array that endPoint2 was found in
+            }
+         }
+
+         if (isEP1Field && isEP2Field) { //both endpoints are fields, implies lack of normalization
+            JOptionPane.showMessageDialog(null, "The Edge Diagrammer file\n" + fileName + "\ncontains composite attributes. Please resolve them and try again.");
+            EdgeConvertGUI.setReadSuccess(false); //this tells GUI not to populate JList components
+            return;
+            //break; //stop processing list of Connectors
+         }
+
+         if (isEP1Table && isEP2Table) { //both endpoints are tables
+            if ((endStyle1.indexOf("many") >= 0) &&
+                    (endStyle2.indexOf("many") >= 0)) { //the connector represents a many-many relationship, implies lack of normalization
+               JOptionPane.showMessageDialog(null, "There is a many-many relationship between tables\n\"" + tables[table1Index].getName() + "\" and \"" + tables[table2Index].getName() + "\"" + "\nPlease resolve this and try again.");
+               EdgeConvertGUI.setReadSuccess(false); //this tells GUI not to populate JList components
+               return;
+               //break; //stop processing list of Connectors
+            } else { //add Figure number to each table's list of related tables
+               tables[table1Index].addRelatedTable(tables[table2Index].getNumFigure());
+               tables[table2Index].addRelatedTable(tables[table1Index].getNumFigure());
+               //continue; //next Connector
+            }
+         }
+
+         if (fieldIndex >=0 && fields[fieldIndex].getTableID() == 0) { //field has not been assigned to a table yet
+            if (isEP1Table) { //endpoint1 is the table
+               tables[table1Index].addNativeField(fields[fieldIndex].getNumFigure()); //add to the appropriate table's field list
+               fields[fieldIndex].setTableID(tables[table1Index].getNumFigure()); //tell the field what table it belongs to
+            } else { //endpoint2 is the table
+               tables[table2Index].addNativeField(fields[fieldIndex].getNumFigure()); //add to the appropriate table's field list
+               fields[fieldIndex].setTableID(tables[table2Index].getNumFigure()); //tell the field what table it belongs to
+            }
+         } else if (fieldIndex >=0) { //field has already been assigned to a table
+            JOptionPane.showMessageDialog(null, "The attribute " + fields[fieldIndex].getName() + " is connected to multiple tables.\nPlease resolve this and try again.");
+            EdgeConvertGUI.setReadSuccess(false); //this tells GUI not to populate JList components
+            return;
+            //break; //stop processing list of Connectors
+         }
    }
    
    public int getNumConnector() {
